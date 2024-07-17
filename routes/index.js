a01320d6e6f7069be578e3ea5cbd5a83b9128bce
ChowdhuryFatema <fatemachowdhury318@gmail.com>
2024-07-17 11:35:57 +0600

    Added Get Single Item API

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 
-let item = [
+let items = [
   { id: 1, name: 'Item1', price: 100 },
   { id: 2, name: 'Item2', price: 200 }
 ]
@@ -10,14 +10,20 @@ let item = [
 // Get Items
 router.get('/api/items', (req, res) => {
   res.set('X-Custom-Header', 'HelloWorld');
-  res.status(200).json(item);
+  res.status(200).json(items);
 })
 
 // Get Single Item
+router.get('/api/items/:id', (req, res) => {
+  const item = items.find(i => i.id === parseInt(req.params.id));
+  if(!item) return res.status(404).send('Item not found')
+  res.status(200).json(item);
+})
 
 // Post Item
-// router.post('/', (req, res) => {
-
+// router.post('/api/items', (req, res) => {
+//   const { name, price } = req.body;
+//   if(!name || !price) return res.status(404).send('')
 // })
 
 
