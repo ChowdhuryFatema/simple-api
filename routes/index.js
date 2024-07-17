b5d54914bed725678c9424aff7f4bab1827c43de
ChowdhuryFatema <fatemachowdhury318@gmail.com>
2024-07-17 12:17:16 +0600

    Added Update Item API

--- routes/index.js
+++ routes/index.js
@@ -7,29 +7,39 @@ let items = [
   { id: 2, name: 'Item2', price: 200 }
 ]
 
-// Get Items
+// Get Items API
 router.get('/api/items', (req, res) => {
   res.set('X-Custom-Header', 'HelloWorld');
   res.status(200).json(items);
 })
 
-// Get Single Item
+// Get Single Item API
 router.get('/api/items/:id', (req, res) => {
   const item = items.find(i => i.id === parseInt(req.params.id));
-  if(!item) return res.status(404).send('Item not found')
+  if (!item) return res.status(404).send('Item not found')
   res.status(200).json(item);
 })
 
-// Post Item
+// Create Item API
 router.post('/api/items', (req, res) => {
   const { name, price } = req.body;
-  if(!name || !price) return res.status(404).send('Invalid request');
+  if (!name || !price) return res.status(404).send('Invalid request');
   const newItem = { id: items.length + 1, name, price }
   items.push(newItem);
   res.status(201).json(newItem);
 })
 
+// Update Item API
+router.put('/api/items/:id', (req, res) => {
+  const item = items.find(i => i.id === parseInt(req.params.id));
+  if (!item) return res.status(404).send('Item not found');
 
+  const updatedItem = req.body;
+  item.name = updatedItem.name ? updatedItem.name : item.name;
+  item.price = updatedItem.price ? updatedItem.price : item.price;
+
+  res.status(200).json(item);
+});
 
 
 /* GET home page. */
